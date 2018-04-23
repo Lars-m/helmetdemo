5b868218713c25cd886cdf1d8c0878761fbbc529
lars-m <lam@cphbusiness.dk>
2018-04-23 12:02:55 +0200

    hpkp

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ const express = require("express");
 const helmet = require("helmet");
 const app = express();
 app.use(helmet());
+var ninetyDaysInSeconds = 7776000
+
+app.use(helmet.hpkp({
+  maxAge: ninetyDaysInSeconds,
+  sha256s: ['xmvvalwaPni4IBbhPzFPPMX6JbHlKqua257FmJsWWto=']
+}))
+
+
 app.get("/",(req,res)=>res.send("Check the Response-headers"));
 app.listen (7654,()=>console.log("Server started, listening on 7654"));
