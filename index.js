63ea39e55f624b89233227a4e065d56386d59889
lars-m <lam@cphbusiness.dk>
2018-04-23 14:54:54 +0200

    index.html

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const express = require("express");
 const helmet = require("helmet");
 const app = express();
 app.use(helmet());
-// var ninetyDaysInSeconds = 7776000
 
+
+
+//This header is set via Nginx, see README.md
+// var ninetyDaysInSeconds = 7776000
 // app.use(helmet.hpkp({
 //   maxAge: ninetyDaysInSeconds,
 //   sha256s: ['xmvvalwaPni4IBbhPzFPPMX6JbHlKqua257FmJsWWto=']
 // }))
 
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    styleSrc: ["'self'", 'maxcdn.bootstrapcdn.com'],
+    fontSrc : ["'self'",' https: data:'],
+  }
+}))
+
+app.use(express.static('public'))
 
 app.get("/",(req,res)=>res.send("Check the Response-headers"));
 app.listen (7654,()=>console.log("Server started, listening on 7654"));
