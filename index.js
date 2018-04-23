1211d03f88231457be0b92292a02631a34f78be4
lars-m <lam@cphbusiness.dk>
2018-04-23 12:11:45 +0200

    removed hpkp

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const helmet = require("helmet");
 const app = express();
 app.use(helmet());
-var ninetyDaysInSeconds = 7776000
+// var ninetyDaysInSeconds = 7776000
 
-app.use(helmet.hpkp({
-  maxAge: ninetyDaysInSeconds,
-  sha256s: ['xmvvalwaPni4IBbhPzFPPMX6JbHlKqua257FmJsWWto=']
-}))
+// app.use(helmet.hpkp({
+//   maxAge: ninetyDaysInSeconds,
+//   sha256s: ['xmvvalwaPni4IBbhPzFPPMX6JbHlKqua257FmJsWWto=']
+// }))
 
 
 app.get("/",(req,res)=>res.send("Check the Response-headers"));
